5abfa883e755f445f915c451959f2106856b7e99
t.decaux <t.decaux@qwant.com>
2016-03-30 23:10:10 +0200

    [docker] Add logging to loggly via syslog

--- bin/provisioning/docker.sh
+++ bin/provisioning/docker.sh
@@ -19,3 +19,5 @@ usermod -aG docker kinoulink
 curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 
 chmod +x /usr/local/bin/docker-compose
+
+echo 'DOCKER_OPTS="--log-driver=syslog --log-opt syslog-address=udp://127.0.0.1:514 --log-opt tag=docker"' >> /etc/default/docker
